05854517909545c2b4a53f68e2a9833e58efe51e
Jack Baron <jackmbaron@gmail.com>
2020-10-05 12:12:18 +0100

    generate command

---
+++ cli/cmd/generate.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/lolPants/markov/cli/pkg/markov"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	generateCmd = &cobra.Command{
+		Use:   "generate",
+		Short: "uses a model from stdin to output new strings to stdout",
+		Run: func(cmd *cobra.Command, args []string) {
+			reader := bufio.NewReader(os.Stdin)
+			writer := bufio.NewWriter(os.Stdout)
+			defer writer.Flush()
+
+			model := markov.NewModel()
+			err := model.ImportReader(reader)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+			}
+
+			lines := viper.GetInt("lines")
+			for i := 0; i < lines; i++ {
+				writer.WriteString(model.Generate())
+				writer.WriteRune('\n')
+			}
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(generateCmd)
+
+	generateCmd.Flags().UintP("lines", "L", 1, "number of lines to generate")
+	viper.BindPFlag("lines", generateCmd.Flags().Lookup("lines"))
+}
