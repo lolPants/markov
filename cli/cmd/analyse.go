18fbbc96ca16d86a2f6bd2eadf66e2c9e04adea9
Jack Baron <jackmbaron@gmail.com>
2020-10-06 09:42:31 +0100

    enable gzip compressing model output

--- cli/cmd/analyse.go
+++ cli/cmd/analyse.go
@@ -2,11 +2,13 @@ package cmd
 
 import (
 	"bufio"
+	"compress/gzip"
 	"fmt"
 	"os"
 
 	"github.com/lolPants/markov/cli/pkg/markov"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -16,6 +18,13 @@ var (
 		Run: func(cmd *cobra.Command, args []string) {
 			if checkIsPipe(os.Stdin) == false {
 				fmt.Fprintln(os.Stderr, "lines must be input via shell redirection or piping")
+
+				os.Exit(1)
+				return
+			}
+
+			if viper.GetBool("gzip-analyse") && checkIsPipe(os.Stdout) == false {
+				fmt.Fprintln(os.Stderr, "output must be piped or redirected when `--gzip` is set")
 
 				os.Exit(1)
 				return
@@ -31,7 +40,15 @@ var (
 				fmt.Fprintln(os.Stderr, err.Error())
 			}
 
-			err = model.ExportWriter(writer)
+			if viper.GetBool("gzip-analyse") {
+				gz := gzip.NewWriter(writer)
+				err = model.ExportWriter(gz)
+
+				gz.Close()
+			} else {
+				err = model.ExportWriter(writer)
+			}
+
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err.Error())
 			}
@@ -41,4 +58,7 @@ var (
 
 func init() {
 	rootCmd.AddCommand(analyseCmd)
+
+	analyseCmd.Flags().BoolP("gzip", "Z", false, "enable gzip compression")
+	viper.BindPFlag("gzip-analyse", analyseCmd.Flags().Lookup("gzip"))
 }
